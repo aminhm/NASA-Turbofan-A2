48228627e53c8b1a47af2415f83fa318972c50d2
NicoNiemela <nico.niemela@hotmail.com>
2025-09-13 21:04:45 +0300

    Create data_import_initial_analysis.py

---
+++ data_import_initial_analysis.py
@@ -0,0 +1,69 @@
+# NASA-Turbofan-A2
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Define column names
+column_names = ['unit_number', 'time_in_cycles', 'op_setting_1', 'op_setting_2', 'op_setting_3'] + [f'sensor_measurement_{i}' for i in range(1, 22)]  # total 26 columns
+
+# Read the data to different datasets
+train_df1 = pd.read_csv('train_FD001.txt', sep='\s+', header=None, names=column_names)
+train_df2 = pd.read_csv('train_FD002.txt', sep='\s+', header=None, names=column_names)
+train_df3 = pd.read_csv('train_FD003.txt', sep='\s+', header=None, names=column_names)
+train_df4 = pd.read_csv('train_FD004.txt', sep='\s+', header=None, names=column_names)
+test_df1 = pd.read_csv('test_FD001.txt', sep='\s+', header=None, names=column_names)
+test_df2 = pd.read_csv('test_FD002.txt', sep='\s+', header=None, names=column_names)
+test_df3 = pd.read_csv('test_FD003.txt', sep='\s+', header=None, names=column_names)
+test_df4 = pd.read_csv('test_FD004.txt', sep='\s+', header=None, names=column_names)
+RUL_df1 = pd.read_csv('RUL_FD001.txt', header=None, names=['remaining_useful_life_FD001'])
+RUL_df2 = pd.read_csv('RUL_FD002.txt', header=None, names=['remaining_useful_life_FD002'])
+RUL_df3 = pd.read_csv('RUL_FD003.txt', header=None, names=['remaining_useful_life_FD003'])
+RUL_df4 = pd.read_csv('RUL_FD004.txt', header=None, names=['remaining_useful_life_FD004'])
+
+# Plot cycles per engine
+train_df1.groupby('unit_number')['time_in_cycles'].max().plot(kind='hist', bins=50)
+plt.title('Distribution of Engine Cycles in the Dataset train_FD001')
+plt.xlabel('Max Cycles per Engine')
+plt.show()
+
+# Check for NaNs
+print(train_df1.isna().sum())
+print(train_df2.isna().sum())
+print(train_df3.isna().sum())
+print(train_df4.isna().sum())
+print(test_df1.isna().sum())
+print(test_df2.isna().sum())
+print(test_df3.isna().sum())
+print(test_df4.isna().sum())
+print(RUL_df1.isna().sum())
+print(RUL_df2.isna().sum())
+print(RUL_df3.isna().sum())
+print(RUL_df4.isna().sum())
+
+# Check for constant values
+constant_cols = [col for col in train_df1.columns if train_df1[col].nunique() <= 1]
+print("train_FD001 Constant Columns:", constant_cols)
+constant_cols = [col for col in train_df2.columns if train_df2[col].nunique() <= 1]
+print("train_FD002 Constant Columns:", constant_cols)
+constant_cols = [col for col in train_df3.columns if train_df3[col].nunique() <= 1]
+print("train_FD003 Constant Columns:", constant_cols)
+constant_cols = [col for col in train_df4.columns if train_df4[col].nunique() <= 1]
+print("train_FD004 Constant Columns:", constant_cols)
+constant_cols = [col for col in test_df1.columns if test_df1[col].nunique() <= 1]
+print("test_FD001 Constant Columns:", constant_cols)
+constant_cols = [col for col in test_df2.columns if test_df2[col].nunique() <= 1]
+print("test_FD002 Constant Columns:", constant_cols)
+constant_cols = [col for col in test_df3.columns if test_df3[col].nunique() <= 1]
+print("test_FD003 Constant Columns:", constant_cols)
+constant_cols = [col for col in test_df4.columns if test_df4[col].nunique() <= 1]
+print("test_FD004 Constant Columns:", constant_cols)
+constant_cols = [col for col in RUL_df1.columns if RUL_df1[col].nunique() <= 1]
+print("RUL_FD001 Constant Columns:", constant_cols)
+constant_cols = [col for col in RUL_df2.columns if RUL_df2[col].nunique() <= 1]
+print("RUL_FD002 Constant Columns:", constant_cols)
+constant_cols = [col for col in RUL_df3.columns if RUL_df3[col].nunique() <= 1]
+print("RUL_FD003 Constant Columns:", constant_cols)
+constant_cols = [col for col in RUL_df4.columns if RUL_df4[col].nunique() <= 1]
+print("RUL_FD004 Constant Columns:", constant_co
