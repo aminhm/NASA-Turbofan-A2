c7bbc4f82bc64b8e68f901b07944e9122041c242
Antonio Oliva <160790965+AntoniooOliva@users.noreply.github.com>
2025-09-14 01:05:06 +0300

    Added dataset

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NASA-Turbofan-A2
 
+To run the project, first you need to do
+```bash
+    pip install -r requirements.txt
+```
--- data_import_initial_analysis.py
+++ data_import_initial_analysis.py
@@ -9,18 +9,18 @@ import seaborn as sns
 column_names = ['unit_number', 'time_in_cycles', 'op_setting_1', 'op_setting_2', 'op_setting_3'] + [f'sensor_measurement_{i}' for i in range(1, 22)]  # total 26 columns
 
 # Read the data to different datasets
-train_df1 = pd.read_csv('train_FD001.txt', sep='\s+', header=None, names=column_names)
-train_df2 = pd.read_csv('train_FD002.txt', sep='\s+', header=None, names=column_names)
-train_df3 = pd.read_csv('train_FD003.txt', sep='\s+', header=None, names=column_names)
-train_df4 = pd.read_csv('train_FD004.txt', sep='\s+', header=None, names=column_names)
-test_df1 = pd.read_csv('test_FD001.txt', sep='\s+', header=None, names=column_names)
-test_df2 = pd.read_csv('test_FD002.txt', sep='\s+', header=None, names=column_names)
-test_df3 = pd.read_csv('test_FD003.txt', sep='\s+', header=None, names=column_names)
-test_df4 = pd.read_csv('test_FD004.txt', sep='\s+', header=None, names=column_names)
-RUL_df1 = pd.read_csv('RUL_FD001.txt', header=None, names=['remaining_useful_life_FD001'])
-RUL_df2 = pd.read_csv('RUL_FD002.txt', header=None, names=['remaining_useful_life_FD002'])
-RUL_df3 = pd.read_csv('RUL_FD003.txt', header=None, names=['remaining_useful_life_FD003'])
-RUL_df4 = pd.read_csv('RUL_FD004.txt', header=None, names=['remaining_useful_life_FD004'])
+train_df1 = pd.read_csv('data/train_FD001.txt', sep=r'\s+', header=None, names=column_names)
+train_df2 = pd.read_csv('data/train_FD002.txt', sep=r'\s+', header=None, names=column_names)
+train_df3 = pd.read_csv('data/train_FD003.txt', sep=r'\s+', header=None, names=column_names)
+train_df4 = pd.read_csv('data/train_FD004.txt', sep=r'\s+', header=None, names=column_names)
+test_df1 = pd.read_csv('data/test_FD001.txt', sep=r'\s+', header=None, names=column_names)
+test_df2 = pd.read_csv('data/test_FD002.txt', sep=r'\s+', header=None, names=column_names)
+test_df3 = pd.read_csv('data/test_FD003.txt', sep=r'\s+', header=None, names=column_names)
+test_df4 = pd.read_csv('data/test_FD004.txt', sep=r'\s+', header=None, names=column_names)
+RUL_df1 = pd.read_csv('data/RUL_FD001.txt', header=None, names=['remaining_useful_life_FD001'])
+RUL_df2 = pd.read_csv('data/RUL_FD002.txt', header=None, names=['remaining_useful_life_FD002'])
+RUL_df3 = pd.read_csv('data/RUL_FD003.txt', header=None, names=['remaining_useful_life_FD003'])
+RUL_df4 = pd.read_csv('data/RUL_FD004.txt', header=None, names=['remaining_useful_life_FD004'])
 
 # Plot cycles per engine
 train_df1.groupby('unit_number')['time_in_cycles'].max().plot(kind='hist', bins=50)
@@ -66,4 +66,4 @@ print("RUL_FD002 Constant Columns:", constant_cols)
 constant_cols = [col for col in RUL_df3.columns if RUL_df3[col].nunique() <= 1]
 print("RUL_FD003 Constant Columns:", constant_cols)
 constant_cols = [col for col in RUL_df4.columns if RUL_df4[col].nunique() <= 1]
-print("RUL_FD004 Constant Columns:", constant_co
+print("RUL_FD004 Constant Columns:", constant_cols)
